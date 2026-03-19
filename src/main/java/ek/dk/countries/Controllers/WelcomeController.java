ccbd5290c6efa1f3ef24014d3cbe19880fb55823
anth328 <anth0008@stud.ek.dk>
2026-03-20 00:26:25 +0100

    Added Thymeleaf template & Homepage

--- src/main/java/ek/dk/countries/Controllers/WelcomeController.java
+++ src/main/java/ek/dk/countries/Controllers/WelcomeController.java
@@ -2,12 +2,16 @@ package ek.dk.countries.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class WelcomeController {
 
     @GetMapping("/")
-    public String welcome() {
-        return "Welcome to the countries website!";
+    public ModelAndView welcome() {
+        ModelAndView mav = new ModelAndView("welcome");
+        mav.addObject("message","Welcome To Countries");
+        return mav;
     }
 }
